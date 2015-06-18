c015fca88d2d55664645ee208f0ade14b0eb858a
nethergrim <c2q9450@gmail.com>
2015-06-18 22:18:42 +0300

    optimized broadcast receivers

--- app/src/main/java/com/nethergrim/bashorg/receiver/XReceiver.java
+++ app/src/main/java/com/nethergrim/bashorg/receiver/XReceiver.java
@@ -3,6 +3,8 @@ package com.nethergrim.bashorg.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 import com.nethergrim.bashorg.App;
 import com.nethergrim.bashorg.Prefs;
@@ -13,6 +15,30 @@ import com.nethergrim.bashorg.web.RunnerService;
  */
 public class XReceiver extends BroadcastReceiver {
 
+
+    public static int TYPE_WIFI = 1;
+    public static int TYPE_MOBILE = 2;
+    public static int TYPE_NOT_CONNECTED = 0;
+
+    public static int getConnectivityStatus(Context context) {
+
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        if (null != activeNetwork) {
+
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
+                return TYPE_WIFI;
+
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE)
+                return TYPE_MOBILE;
+        }
+        return TYPE_NOT_CONNECTED;
+
+
+    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -30,10 +56,8 @@ public class XReceiver extends BroadcastReceiver {
     }
 
     private void checkPossibilityToFetchData() {
-        if (Prefs.isCharging() && App.isOnline()) {
+        if (Prefs.isCharging() && getConnectivityStatus(App.getInstance()) == TYPE_WIFI) {
             RunnerService.triggerFetching();
         }
     }
-
-
 }
