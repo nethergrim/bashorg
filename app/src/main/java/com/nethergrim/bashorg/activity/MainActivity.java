f4fa00b3884e31886a6524f6d3939a4a304d92a0
nethergrim <c2q9450@gmail.com>
2015-08-25 17:31:25 +0300

    [BSRG-5] - Added VK ads to SnackBar

--- app/src/main/java/com/nethergrim/bashorg/activity/MainActivity.java
+++ app/src/main/java/com/nethergrim/bashorg/activity/MainActivity.java
@@ -7,28 +7,54 @@ import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+
 import com.github.clans.fab.FloatingActionButton;
 import com.nethergrim.bashorg.Constants;
 import com.nethergrim.bashorg.R;
 import com.nethergrim.bashorg.adapter.FragmentAdapter;
 import com.nethergrim.bashorg.db.DB;
-import com.nethergrim.bashorg.fragment.*;
+import com.nethergrim.bashorg.fragment.AbyssFragment;
+import com.nethergrim.bashorg.fragment.BestQuotesFragment;
+import com.nethergrim.bashorg.fragment.LastQuotesFragment;
+import com.nethergrim.bashorg.fragment.LikedQuotesFragment;
+import com.nethergrim.bashorg.fragment.RandomQuotesFragment;
+import com.nethergrim.bashorg.utils.AdsHelper;
 import com.nethergrim.bashorg.utils.FileUtils;
+import com.nethergrim.bashorg.utils.Prefs;
 import com.nethergrim.bashorg.utils.ThemeUtils;
+
 import org.json.JSONArray;
 
 
-public class MainActivity extends FragmentActivity implements TabLayout.OnTabSelectedListener, View.OnClickListener {
+public class MainActivity extends FragmentActivity
+        implements TabLayout.OnTabSelectedListener, View.OnClickListener {
 
     private ViewPager pager;
     private FragmentAdapter adapter;
     private TabLayout tabs;
     private IntentFilter filter = new IntentFilter(Constants.ACTION_SHARE_QUOTE);
     private BroadcastReceiver receiver;
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        Integer id = (Integer) tab.getTag();
+        pager.setCurrentItem(id, true);
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +67,54 @@ public class MainActivity extends FragmentActivity implements TabLayout.OnTabSel
         pager.setAdapter(adapter);
         pager.setOffscreenPageLimit(6);
         tabs = (TabLayout) findViewById(R.id.tabs);
-        FloatingActionButton mFab = (FloatingActionButton) findViewById(R.id.fab);
+        final FloatingActionButton mFab = (FloatingActionButton) findViewById(R.id.fab);
         mFab.setOnClickListener(this);
         loadFragments();
         initTabs();
-        if (DB.getInstance().getCountOfLoadedQuotes() < 52000) {
+        if (DB.getInstance().getCountOfLoadedQuotes() < 52000) { // zip file not decompressed
             decompressZipFileAndPersistToDb();
         }
+        mFab.animate().translationY(0).setDuration(250).start();
+        if (Prefs.isFirstAppLauch()) {
+            View parentLayout = findViewById(R.id.root);
+            Snackbar.make(parentLayout, R.string.our_vk_group, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(
+                            R.string.go, new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    AdsHelper.showVkGroupAds(v.getContext());
+                                    Prefs.setFirstAppLaunch(false);
+                                    mFab.animate().translationY(0).setDuration(250).start();
+                                }
+                            })
+                    .show();
+            mFab.invalidate();
+            mFab.animate().translationY(-48 * Constants.density).setDuration(250).start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String quote = intent.getStringExtra(Constants.EXTRA_QUOTE);
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, quote);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+
+            }
+        };
+        registerReceiver(receiver, filter);
     }
 
     private void decompressZipFileAndPersistToDb() {
@@ -55,8 +122,10 @@ public class MainActivity extends FragmentActivity implements TabLayout.OnTabSel
             @Override
             public void run() {
                 Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
-                FileUtils.unpackAssetFileAndUnzip(FileUtils.BASHORG_JSON_FILE_NAME + FileUtils.ZIP_FILE_POSTFIX);
-                JSONArray bashorgBase = FileUtils.getJsonArrayFromDisk(getFilesDir() + "/" + FileUtils.BASHORG_JSON_FILE_NAME);
+                FileUtils.unpackAssetFileAndUnzip(
+                        FileUtils.BASHORG_JSON_FILE_NAME + FileUtils.ZIP_FILE_POSTFIX);
+                JSONArray bashorgBase = FileUtils.getJsonArrayFromDisk(
+                        getFilesDir() + "/" + FileUtils.BASHORG_JSON_FILE_NAME);
                 if (bashorgBase != null) {
                     DB.getInstance().persist(bashorgBase);
                 }
@@ -87,46 +156,6 @@ public class MainActivity extends FragmentActivity implements TabLayout.OnTabSel
         tabs.addTab(tabs.newTab().setText(R.string.best_of_abyss).setTag(Integer.valueOf(4)));
         tabs.setOnTabSelectedListener(this);
         pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabs));
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        receiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                String quote = intent.getStringExtra(Constants.EXTRA_QUOTE);
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, quote);
-                sendIntent.setType("text/plain");
-                startActivity(sendIntent);
-
-            }
-        };
-        registerReceiver(receiver, filter);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unregisterReceiver(receiver);
-    }
-
-    @Override
-    public void onTabSelected(TabLayout.Tab tab) {
-        Integer id = (Integer) tab.getTag();
-        pager.setCurrentItem(id, true);
-    }
-
-    @Override
-    public void onTabUnselected(TabLayout.Tab tab) {
-
-    }
-
-    @Override
-    public void onTabReselected(TabLayout.Tab tab) {
-
     }
 
     @Override
